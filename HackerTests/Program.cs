c0751911c5b0b79448d8504aa2c9622820a0b7a3
Zachary Inkster <z.j.inkster@gmail.com>
2021-03-20 17:56:19 -0500

    Algorithm that compares to lists of integers in same positions to determine who has the higher number. Higher number is assigned a point. Returns a list of both players points.

--- HackerTests/Program.cs
+++ HackerTests/Program.cs
@@ -11,19 +11,51 @@ namespace HackerTests
     {
         static void Main(string[] args)
         {
-            int simpleArraySum(int[] arg)
+            //Simple algorith for running through an array and adding up all members
+            ////int simpleArraySum(int[] arg)
+            ////{
+            ////    int sum = 0;
+            ////    int arraySize = arg.Length;
+            ////    for (int i = 0; i < arraySize; i++)
+            ////    {
+            ////        Console.WriteLine(arg[i]);
+            ////        sum += arg[i];
+            ////    }
+            ////    return sum;
+            ////}
+            ////int[] array = new int[] { 3, 4, 5, 6, 7, 8 };
+            ////Console.WriteLine(simpleArraySum(array));
+            ////Console.ReadLine();
+            //Algorithm comparing two arrays and assigning points to the array with the higher number in the corresponding position
+            List<int> compareTriplets(List<int> a, List<int> b)
             {
-                int sum = 0;
-                int arraySize = arg.Length;
-                for (int i = 0; i < arraySize; i++)
+                int bob = 0;
+                int alice = 0;
+
+                for (int i = 0; i < a.Count; i++)
                 {
-                    Console.WriteLine(arg[i]);
-                    sum += arg[i];
+                    if (a[i] > b[i])
+                    {
+                        alice += 1;
+                    }
+                    else if (a[i] < b[i])
+                    {
+                        bob += 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Its a tie, no points");
+                    }
                 }
-                return sum;
+                int[] scores = new int[2];
+                scores[0] = alice;
+                scores[1] = bob;
+                return scores.ToList();
+
             }
-            int[] array = new int[] { 3, 4, 5, 6, 7, 8 };
-            Console.WriteLine(simpleArraySum(array));
+            List<int> a1 = new List<int>() { 5, 6, 7 };
+            List<int> a2 = new List<int>() { 3, 6, 10 };
+            Console.WriteLine(compareTriplets(a1, a2));
             Console.ReadLine();
         }   
             
